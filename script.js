a5828298fd711bf2f6d09455871410740f532d3b
mohammed-husban <22015295011@std.bau.edu.jo>
2024-06-15 22:01:35 +0300

    Registration Form

--- script.js
+++ script.js
@@ -17,17 +17,6 @@ function validatePassword(password) {
     special.classList.toggle('invalid', !/[!@#$%^&*(),.?":{}|<>]/.test(password));
 }
 
-function togglePasswordVisibility() {
-    const passwordInput = document.getElementById('password');
-    const showPasswordCheckbox = document.getElementById('showPassword');
-    
-    if (showPasswordCheckbox.checked) {
-        passwordInput.type = 'text';
-    } else {
-        passwordInput.type = 'password';
-    }
-}
-
 function checkUsernameExists(username) {
     return new Promise(resolve => {
         setTimeout(() => {
